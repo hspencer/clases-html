aa10e0f7cb33547d6da8645bcfaf39a5fb8d78d3
Herbert Spencer <herbert.spencer@gmail.com>
2015-07-21 09:58:57 -0300

    init

---
+++ js/menu.js
@@ -0,0 +1,14 @@
+/*
+*
+* menu
+*
+*/
+
+jQuery(document).ready(function() {
+	jQuery('.toggle-nav').click(function(e) {
+		jQuery(this).toggleClass('active');
+		jQuery('.menu ul').toggleClass('active');
+
+		e.preventDefault();
+	});
+});
